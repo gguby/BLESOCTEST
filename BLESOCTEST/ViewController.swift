24c7e397ba69f34b27bd776c4dc4206cc032aeac
정원식(Wonsik Jung)/Media솔루션사업팀/SKP <gguby@sk.com>
2022-05-23 10:08:05 +0900

    Initial Commit

---
+++ BLESOCTEST/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BLESOCTEST
+//
+//  Created by 정원식(Wonsik Jung)/Media Integration팀/SKP on 2022/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
